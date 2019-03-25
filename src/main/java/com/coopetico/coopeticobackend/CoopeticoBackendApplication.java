934d1c852eab7e8e1dcc1f3d8c0774673371663a
Jose David Vargas <josed1608@gmail.com>
2019-03-24 18:07:40 -0600

    Project initialization with the basic Spring Boot application and the JPA configuration for using the local MySQL server

---
+++ src/main/java/com/coopetico/coopeticobackend/CoopeticoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.coopetico.coopeticobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopeticoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopeticoBackendApplication.class, args);
+	}
+
+}
