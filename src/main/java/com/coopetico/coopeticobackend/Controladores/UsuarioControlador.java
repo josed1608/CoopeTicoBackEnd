2430dac1c5cc46710498e0340689e3374966fdfa
Kevin Jiménez Álvarez <kevinja9608@gmail.com>
2019-04-02 10:04:41 -0600

    Borrar JPARepository de controlador

--- src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
@@ -1,7 +1,5 @@
 package com.coopetico.coopeticobackend.Controladores;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +7,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-import com.coopetico.coopeticobackend.repositorios.UsuariosRepositorio;
-
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/usuarios") // This means URL's start with /demo (after Application path)
 public class UsuarioControlador {
-    private JpaRepository UsuariosRepositorio;
 
-    
+
+
 
 }
