66d61a0ca015613559d733b63584f24db704b241
Kevin Jiménez Álvarez <kevinja9608@gmail.com>
2019-04-02 10:27:00 -0600

    Agregar configuración de seguridad

---
+++ src/main/java/com/coopetico/coopeticobackend/security/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.coopetico.coopeticobackend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //@formatter:off
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/usuarios/restablecerContrasena").permitAll();
+        //@formatter:on
+    }
+}
