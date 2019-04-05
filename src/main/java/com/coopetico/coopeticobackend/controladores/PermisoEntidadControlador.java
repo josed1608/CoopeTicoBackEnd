b18ab9aeb0bde9b8aef46ad85a3774807395bc0c
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-04-04 21:10:43 -0600

    Agrego el controlador de Permisos y modifico el Query de findAll() porque consultaba las demás tablas (EAGER)

--- src/main/java/com/coopetico/coopeticobackend/controladores/PermisoEntidadControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/PermisoEntidadControlador.java
@@ -1,27 +1,28 @@
 package com.coopetico.coopeticobackend.controladores;
+//Programador: Jefferson Alvarez
+//Fecha: 04/04/2019
+//Version: 0.1
+//Controlador de la entidad Permisos que contiene los permisos existentes en el sistema.
 
 import com.coopetico.coopeticobackend.entidades.PermisoEntidad;
-import com.coopetico.coopeticobackend.repositorios.PermisosGruposRepositorio;
+import com.coopetico.coopeticobackend.repositorios.PermisosRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.coopetico.coopeticobackend.entidades.PermisosGrupoEntidad;
+
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
-public class PermisoGrupoEntidadControlador {
+@RequestMapping("/coopetico")
+public class PermisoEntidadControlador {
 
     @Autowired
-    PermisosGruposRepositorio repoPermisos;
-
+    PermisosRepositorio permisosRepo;
 
     @GetMapping("/permisos")
-    public List<PermisosGrupoEntidad> getPermisos(){
-
-        return repoPermisos.findAll();
-
+    public List<PermisoEntidad> getPermisos(){
+        return permisosRepo.findAll();
     }
+
 }
--- src/main/java/com/coopetico/coopeticobackend/repositorios/PermisosRepositorio.java
+++ src/main/java/com/coopetico/coopeticobackend/repositorios/PermisosRepositorio.java
@@ -2,7 +2,13 @@ package com.coopetico.coopeticobackend.repositorios;
 
 import com.coopetico.coopeticobackend.entidades.PermisoEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PermisosRepositorio extends JpaRepository<PermisoEntidad, String> {
 
+        @Override
+        @Query("select p.pkId, p.descripcion from PermisoEntidad p ")
+        List<PermisoEntidad> findAll();
 }
