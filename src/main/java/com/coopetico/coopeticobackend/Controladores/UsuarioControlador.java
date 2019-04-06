c48cc55b4acbbf46c2e821d1f1bc9e2c343dad54
joe <rementeriajoseph@gmail.com>
2019-04-06 11:58:07 -0600

    base para obtener usuario

--- src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
@@ -1,19 +1,53 @@
 package com.coopetico.coopeticobackend.Controladores;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.coopetico.coopeticobackend.entidades.ClienteEntidad;
+import com.coopetico.coopeticobackend.entidades.UsuarioEntidad;
+import com.coopetico.coopeticobackend.repositorios.UsuariosRepositorio;
+import com.coopetico.coopeticobackend.repositorios.ClientesRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;k;
 
-
-@Controller    // This means that this class is a Controller
-@RequestMapping(path="/usuarios") // This means URL's start with /demo (after Application path)
+/**
+ * TO_DO: comentar según las convenciones
+ */
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioControlador {
-    @GetMapping(path="/restablecerContrasena")
-    public @ResponseBody String addNewUser () {
 
-        return "Hola joe";
+    private final UsuariosRepositorio USUARIOS;
+    private final ClientesRepositorio CLIENTES;
+
+    @Autowired
+    public UsuarioControlador(UsuariosRepositorio users, ClientesRepositorio clnt) {
+        this.USUARIOS = users;
+        this.CLIENTES = clnt;
+    }
+
+    /*
+     * TO_DO: comentar según las convenciones.
+     */
+    @GetMapping(path="/obtener_usuario")
+    public @ResponseBody String/*UsuarioEntidad*/ obtener_usuario (String correo) {
+        String resultado = null;
+        try {
+            UsuarioEntidad usuario_entidad = this.USUARIOS.findById(correo).orElseThrow(() -> new UsernameNotFoundException("no_usuario"));
+            ClienteEntidad cliente_entidad = this.CLIENTES.findById(correo).orElseThrow(() -> new UsernameNotFoundException("no_cliente"));
+            // result = usuario_entidad;
+            resultado = "<p>";
+            resultado +="Nombre: " + usuario_entidad.getNombre();
+            resultado += "</br>Apellidos: " + usuario_entidad.getApellidos();
+            resultado += "</br>Telefono: " + usuario_entidad.getTelefono();
+            resultado += "</p>";
+        } catch (Exception e) {
+            String mensaje = "ERROR no manejado";
+            if (e.getMessage() == "no_usuario") {
+                mensaje = "El usuario no existe";
+            } else if (e.getMessage() == "no_cliente") {
+                mensaje = "El usuario no un cliente";
+            }
+        }
+        return resultado;
     }
 }
