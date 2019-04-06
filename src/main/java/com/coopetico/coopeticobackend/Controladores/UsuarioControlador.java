a944fa08b276beb3a1d3eb7d237c4ac0f1a0f894
joe <rementeriajoseph@gmail.com>
2019-04-06 13:13:14 -0600

    El método ahora devuelve una entidad y no una string

--- src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/Controladores/UsuarioControlador.java
@@ -7,7 +7,7 @@ import com.coopetico.coopeticobackend.repositorios.ClientesRepositorio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;k;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * TO_DO: comentar según las convenciones
@@ -29,24 +29,25 @@ public class UsuarioControlador {
      * TO_DO: comentar según las convenciones.
      */
     @GetMapping(path="/obtener_usuario")
-    public @ResponseBody String/*UsuarioEntidad*/ obtener_usuario (String correo) {
-        String resultado = null;
+    public @ResponseBody UsuarioEntidad/*TO_TO: String*/ obtener_usuario (String correo) {
+        UsuarioEntidad resultado = null;
         try {
             UsuarioEntidad usuario_entidad = this.USUARIOS.findById(correo).orElseThrow(() -> new UsernameNotFoundException("no_usuario"));
             ClienteEntidad cliente_entidad = this.CLIENTES.findById(correo).orElseThrow(() -> new UsernameNotFoundException("no_cliente"));
-            // result = usuario_entidad;
-            resultado = "<p>";
-            resultado +="Nombre: " + usuario_entidad.getNombre();
-            resultado += "</br>Apellidos: " + usuario_entidad.getApellidos();
-            resultado += "</br>Telefono: " + usuario_entidad.getTelefono();
-            resultado += "</p>";
+            resultado = usuario_entidad;
+            // resultado = "<p>";
+            // resultado +="Nombre: " + usuario_entidad.getNombre();
+            // resultado += "</br>Apellidos: " + usuario_entidad.getApellidos();
+            // resultado += "</br>Telefono: " + usuario_entidad.getTelefono();
+            // resultado += "</p>";
         } catch (Exception e) {
-            String mensaje = "ERROR no manejado";
-            if (e.getMessage() == "no_usuario") {
-                mensaje = "El usuario no existe";
-            } else if (e.getMessage() == "no_cliente") {
-                mensaje = "El usuario no un cliente";
-            }
+            // TO_DO: comment all this part.
+            // String mensaje = "ERROR no manejado";
+            // if (e.getMessage() == "no_usuario") {
+            //     mensaje = "El usuario no existe";
+            // } else if (e.getMessage() == "no_cliente") {
+            //     mensaje = "El usuario no un cliente";
+            // }
         }
         return resultado;
     }
