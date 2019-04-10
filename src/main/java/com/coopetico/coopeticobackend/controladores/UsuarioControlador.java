b5ba0a1f357451a0d4c8c3a60851dcb89e8db61a
Kevin Jiménez Álvarez <kevinja9608@gmail.com>
2019-04-09 21:50:59 -0600

    Email validation

--- src/main/java/com/coopetico/coopeticobackend/controladores/UsuarioControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/UsuarioControlador.java
@@ -6,14 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.constraints.Email;
+
 
 @Controller    // This means that this class is a Controller
 @RequestMapping(path="/usuarios") // This means URL's start with /demo (after Application path)
+@Validated
 public class UsuarioControlador {
     @Autowired
     TokensRecuperacionContrasenaServicioImpl tokensServicio;
@@ -21,7 +25,7 @@ public class UsuarioControlador {
     EmailServiceImpl mail ;
 
     @GetMapping(path="/contrasenaToken")
-    public @ResponseBody ResponseEntity recuperarContrasena (@RequestParam("correo") String correo) {
+    public @ResponseBody ResponseEntity recuperarContrasena (@Email @RequestParam("correo") String correo) {
         String token = tokensServicio.insertarToken(correo);
         if (token == null){
             return new ResponseEntity(HttpStatus.NOT_FOUND );
