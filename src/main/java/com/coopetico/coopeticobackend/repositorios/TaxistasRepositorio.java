0f7540acbc829575458c3ff3d0b63168bef1f6f1
christofer <christofer.rodriguez@ucr.ac.cr>
2019-04-10 18:57:12 -0600

    Se agrega cross para que pueda acceder el frontend

--- src/main/java/com/coopetico/coopeticobackend/repositorios/TaxistasRepositorio.java
+++ src/main/java/com/coopetico/coopeticobackend/repositorios/TaxistasRepositorio.java
@@ -2,7 +2,9 @@ package com.coopetico.coopeticobackend.repositorios;
 
 import com.coopetico.coopeticobackend.entidades.TaxistaEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 public interface TaxistasRepositorio extends JpaRepository<TaxistaEntidad, String> {
 
 }
