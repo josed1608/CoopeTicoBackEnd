d4783369f427c961b7e3a69f1ee72b34260f8cbe
Jefferson Alvarez <jeffalvarez08@gmail.com>
2019-04-11 03:00:53 -0600

    Cambio la manera en solicitar los permisos y agrego el ignoreJson de la relacion asociada

--- src/main/java/com/coopetico/coopeticobackend/entidades/PermisoEntidad.java
+++ src/main/java/com/coopetico/coopeticobackend/entidades/PermisoEntidad.java
@@ -1,6 +1,7 @@
 package com.coopetico.coopeticobackend.entidades;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
@@ -13,6 +14,7 @@ import java.util.Objects;
 public class PermisoEntidad {
     private int pkId;
     private String descripcion;
+    @JsonIgnore
     private Collection<PermisosGrupoEntidad> permisosGruposByPkId;
 
     public PermisoEntidad(int pkId, String descripcion, Collection<PermisosGrupoEntidad> permisosGruposByPkId) {
--- src/main/java/com/coopetico/coopeticobackend/repositorios/PermisosRepositorio.java
+++ src/main/java/com/coopetico/coopeticobackend/repositorios/PermisosRepositorio.java
@@ -11,9 +11,5 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface PermisosRepositorio extends JpaRepository<PermisoEntidad, Integer> {
-
-        @Override
-        @Query("select p.pkId, p.descripcion from PermisoEntidad p ")
-        List<PermisoEntidad> findAll();
-
+        
 }
