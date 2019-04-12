860a50d7f2ce64f3faa0612922b0a4d370de89e0
geraldvindas <gerald.vindas@ucr.ac.cr>
2019-04-11 21:30:54 -0600

    Se agrega CRUD administrador

--- src/main/java/com/coopetico/coopeticobackend/controladores/UsuarioControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/UsuarioControlador.java
@@ -1,7 +1,9 @@
 package com.coopetico.coopeticobackend.controladores;
 
+import com.coopetico.coopeticobackend.entidades.GrupoEntidad;
 import com.coopetico.coopeticobackend.entidades.UsuarioEntidad;
 import com.coopetico.coopeticobackend.repositorios.UsuariosRepositorio;
+import com.coopetico.coopeticobackend.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,17 +12,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
-@Controller    // This means that this class is a Controller
-@RequestMapping(path="/usuarios") // This means URL's start with /demo (after Application path)
+
+@RestController
+@RequestMapping(path="/usuarios")
 public class UsuarioControlador {
 
     private UsuariosRepositorio usuariosRepositorio;
-
     private PasswordEncoder encoder;
+    private UsuarioServicio usuarioServicio;
 
     @Autowired
-    public UsuarioControlador(UsuariosRepositorio usuariosRepositorio, PasswordEncoder encoder) {
+    public UsuarioControlador(UsuariosRepositorio usuariosRepositorio, PasswordEncoder encoder, UsuarioServicio servicio) {
+        this.usuarioServicio = servicio;
         this.usuariosRepositorio = usuariosRepositorio;
         this.encoder = encoder;
     }
@@ -36,6 +41,49 @@ public class UsuarioControlador {
         //Actualizar el usuario
         usuariosRepositorio.save(usuarioEntidad);
         return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+
+
+    //TODO ver lo de acoplamiento y cohesion
+    @PostMapping("/usuarios")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioEntidad crearUsuario(@RequestBody UsuarioEntidad usuario){
+        String grupo = usuario.getGrupoByIdGrupo().getPkId();
+        return usuarioServicio.agregarUsuario(usuario, grupo);
+    }
+
+    @GetMapping("/usuarios/{id}")
+    public UsuarioEntidad obtenerUsuarioPorId(@PathVariable String id){
+        return usuarioServicio.usuarioPorCorreo(id).get();
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void eliminarUsuarioPorId(@PathVariable String id){
+        usuarioServicio.eliminar(id);
+    }
+
+    @GetMapping("/usuarios")
+    public List<UsuarioEntidad> obtenerUsuarios(){
+        return usuarioServicio.obtenerUsuarios();
+    }
+
+    @GetMapping("/grupo")
+    public List<UsuarioEntidad> obtenerUsuariosPorGrupo(@RequestBody GrupoEntidad grupoEntidad){
+        return usuarioServicio.obtenerUsuariosPorGrupo(grupoEntidad);
+    }
+
+    @PutMapping("/usuarios/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioEntidad actualizar(@RequestBody UsuarioEntidad usuario, @PathVariable String id){
+        UsuarioEntidad usuarioTemporal = this.usuarioServicio.usuarioPorCorreo(id).get();
+        usuarioTemporal.setApellidos(usuario.getApellidos());
+        usuarioTemporal.setNombre(usuario.getNombre());
+        usuarioTemporal.setTelefono(usuario.getTelefono());
+
+        return usuarioServicio.agregarUsuario(usuarioTemporal, usuarioTemporal.getPkCorreo());
     }
 }
 
--- src/main/java/com/coopetico/coopeticobackend/servicios/UsuarioServicio.java
+++ src/main/java/com/coopetico/coopeticobackend/servicios/UsuarioServicio.java
@@ -1,7 +1,9 @@
 package com.coopetico.coopeticobackend.servicios;
 
+import com.coopetico.coopeticobackend.entidades.GrupoEntidad;
 import com.coopetico.coopeticobackend.entidades.UsuarioEntidad;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioServicio {
@@ -10,8 +12,9 @@ public interface UsuarioServicio {
      *
      * @param usuarioSinGrupo Entidad del usuario sin la referencia al grupo
      * @param grupoId id del grupo de permisos al que se va a linkear el usuario
+     * @return Retorna el usuario agregado
      */
-    void agregarUsuario(UsuarioEntidad usuarioSinGrupo, String grupoId);
+    UsuarioEntidad agregarUsuario(UsuarioEntidad usuarioSinGrupo, String grupoId);
 
     /**
      * Encuentra un usuario por correo
@@ -20,4 +23,26 @@ public interface UsuarioServicio {
      * @return retorna el usuario en un Optional para poder aplicar lógica en caso de que no lo encuentre
      */
     Optional<UsuarioEntidad> usuarioPorCorreo(String correo);
+
+
+    /**
+     * Permite obtener todos los usuarios
+     * @return Retorna una lista de usuarios
+     */
+    List<UsuarioEntidad> obtenerUsuarios();
+
+    /**
+     * Permite obtener todos los usuarios de un grupo
+     * @return Retorna la lista de usuarios que pertenecen al grupo
+     */
+    List<UsuarioEntidad> obtenerUsuariosPorGrupo(GrupoEntidad grupo);
+
+
+    /**
+     * Elimina un usuario
+     * @param correo Usuario a eliminar
+     */
+    void eliminar(String correo);
+
+
 }
