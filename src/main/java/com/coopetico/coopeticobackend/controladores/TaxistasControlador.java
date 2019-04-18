85e93b1d3fc66498e208c67a5de07fb533cfbf95
christofer <christofer.rodriguez@ucr.ac.cr>
2019-04-17 18:59:24 -0600

    Cambio en el requestMapping de taxistas

--- src/main/java/com/coopetico/coopeticobackend/controladores/TaxistasControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/TaxistasControlador.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/taxistas")
 public class TaxistasControlador {
 
     /**
