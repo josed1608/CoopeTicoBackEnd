6ea96419519675a054a4ea21e35dee925281add8
HanniaAguilar <hannia.aguilar@ucr.ac.cr>
2019-04-21 09:20:33 -0600

    Pruebas de usuario

---
+++ src/test/java/com/coopetico/coopeticobackend/controladores/unit/UsuarioControladorUnitTest.java
@@ -0,0 +1,12 @@
+package com.coopetico.coopeticobackend.controladores.unit;
+
+/**
+ Pruebas de unidad del UsuarioControlador
+ @author      Hannia Aguilar Salas
+ @since       21-04-2019
+ @version:    1.0
+ */
+
+
+public class UsuarioControladorUnitTest {
+}
