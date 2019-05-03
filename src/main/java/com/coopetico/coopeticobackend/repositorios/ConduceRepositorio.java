486bfd46b53cbb31cbb0a042933d0ac527c15262
Jose David Vargas <josed1608@gmail.com>
2019-05-02 19:15:39 -0600

    Se agregó el repo para la nueva tabla de conduce

---
+++ src/main/java/com/coopetico/coopeticobackend/repositorios/ConduceRepositorio.java
@@ -0,0 +1,8 @@
+package com.coopetico.coopeticobackend.repositorios;
+
+import com.coopetico.coopeticobackend.entidades.ConduceEntidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConduceRepositorio extends JpaRepository<ConduceEntidad, String> {
+
+}
--- src/main/java/com/coopetico/coopeticobackend/repositorios/OperadorRepositorio.java
+++ src/main/java/com/coopetico/coopeticobackend/repositorios/OperadorRepositorio.java
@@ -3,6 +3,6 @@ package com.coopetico.coopeticobackend.repositorios;
 import com.coopetico.coopeticobackend.entidades.OperadorEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CoopeticosRepositorio extends JpaRepository<OperadorEntidad, String> {
+public interface OperadorRepositorio extends JpaRepository<OperadorEntidad, String> {
 
 }
