18bf2610760f361c3e661aac89cbeca9a909a781
geraldvindas <gerald.vindas@ucr.ac.cr>
2019-05-13 15:10:56 -0600

    Se agrega metodo que asocia estructura de Marco

--- src/main/java/com/coopetico/coopeticobackend/controladores/UbicacionTaxiControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/UbicacionTaxiControlador.java
@@ -1,15 +1,17 @@
 package com.coopetico.coopeticobackend.controladores;
 
 
+import com.coopetico.coopeticobackend.entidades.TaxiEntidad;
 import com.coopetico.coopeticobackend.entidades.TaxiTemporal;
+import com.coopetico.coopeticobackend.servicios.TaxisServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @CrossOrigin( origins = {"http://localhost:4200"})
 @RestController
@@ -21,6 +23,11 @@ import java.util.List;
 public class UbicacionTaxiControlador {
     // Boolena para simular que los taxis se mueven
     boolean primero = true;
+
+    HashMap<String, Double> ubicacionTaxis;
+
+    @Autowired
+    TaxisServicio taxisServicio;
 
     /**
      * Metodo para obtener una lista de taxis
@@ -53,4 +60,38 @@ public class UbicacionTaxiControlador {
         primero = !primero;
         return taxis;
     }
+
+
+    /**
+     * Metodo que se encarga de asociar cada taxi de la estructura de las ubicaciones de los taxis
+     * con una clase de TaxiTemporal.
+     * @return Lista TaxiTemporal con sus caracteristicas necesarias para el FE
+     */
+    public List<TaxiTemporal> asociarTaxis(){
+        List<TaxiTemporal> listaUbicacionTaxi = new ArrayList<>();
+        List<TaxiEntidad> listaTaxis = taxisServicio.consultar();
+        Set<String> ubicacionesTaxi = ubicacionTaxis.keySet();
+        for (String placa : ubicacionesTaxi) {
+            // TODO Se necesita realizar una clase para
+            // TODO usar estructura de Marco
+            TaxiEntidad taxi = obtenerTaxi(listaTaxis, placa);
+            listaUbicacionTaxi.add(new TaxiTemporal(placa,ubicacionTaxis.get(placa), ubicacionTaxis.get(placa), taxi.getClase(),  true, taxi.getDatafono()));
+        }
+        return  listaUbicacionTaxi;
+    }
+
+    /**
+     * Metodo que obtiene el taxi de una lista
+     * @param taxis Lista de taxis
+     * @param placa Placa a buscar
+     * @return El objeto del taxi
+     */
+    public TaxiEntidad obtenerTaxi(List<TaxiEntidad> taxis, String placa){
+        for (TaxiEntidad taxi : taxis) {
+            if(taxi.getPkPlaca().equals(placa))
+                return taxi;
+        }
+        return null;
+    }
+
 }
