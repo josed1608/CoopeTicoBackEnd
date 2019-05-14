0d8584ee93dc63e97ca31e8b3f9917439896dd51
Marco <marco.venegas98@hotmail.com>
2019-05-13 20:10:11 -0600

    Creo la interfaz para el servicio de la ubicacion de los taxistas

---
+++ src/main/java/com/coopetico/coopeticobackend/servicios/UbicacionTaxistasServicio.java
@@ -0,0 +1,32 @@
+package com.coopetico.coopeticobackend.servicios;
+
+/**
+ Interfaz del servicio de la ubicación de los taxistas en tiempo real.
+ @author      Marco Venegas
+ @since       13-05-2019
+ @version:    1.0
+ */
+
+import com.google.maps.model.LatLng;
+import org.springframework.data.util.Pair;
+
+public interface UbicacionTaxistasServicio {
+    /**
+     * Inserta un taxista y su ubicación a la estructura de datos.
+     * Si ya existía dentro de la estructura, lo actualiza con la nueva ubicación.
+     * @param taxista Par con el id del taxista y la latitud,longitud de su ubicación
+     */
+    void upsertTaxista(Pair<String, LatLng> taxista);
+
+    /**
+     * Elimina un taxista y su ubicación a la estructura de datos.
+     * @param taxistaId Id del taxista que se eliminará de la estructura
+     */
+    void eliminarTaxista(String taxistaId);
+
+    /**
+     * Consulta la  ubicación actual de un taxista.
+     * @param taxistaId Id del taxista que se consultará
+     */
+    LatLng consultarUbicacion(String taxistaId);
+}
