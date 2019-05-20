a578e8f2143f0f4e360703c06025660eefb7a707
HanniaAguilar <hannia.aguilar@ucr.ac.cr>
2019-05-20 15:10:57 -0600

    Pruebas del controlador de viaje. Parte de consultar viaje.

--- src/test/java/com/coopetico/coopeticobackend/controladores/integration/ViajesControladorIntegrationTest.java
+++ src/test/java/com/coopetico/coopeticobackend/controladores/integration/ViajesControladorIntegrationTest.java
@@ -7,15 +7,85 @@ package com.coopetico.coopeticobackend.controladores.integration;
  @version:    1.0
  */
 
+import com.coopetico.coopeticobackend.controladores.ViajeControlador;
+import com.coopetico.coopeticobackend.entidades.UsuarioTemporal;
+import com.coopetico.coopeticobackend.entidades.ViajeEntidadTemporal;
+import com.coopetico.coopeticobackend.entidades.bd.*;
+import com.coopetico.coopeticobackend.servicios.UsuarioServicio;
+import com.coopetico.coopeticobackend.servicios.ViajesServicio;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ViajesControladorIntegrationTest {
 
+    @Autowired
+    protected WebApplicationContext wac;
 
+    // Mock del mvc
+    private MockMvc mockMvc;
+
+    // Beans de las inyecciones de dependencias
+    @Autowired
+    ViajeControlador viajesControlador;
+
+    @Before
+    public void setup() {
+        this.mockMvc = standaloneSetup(viajesControlador).build();
+    }
+
+    /**
+     * Test de obtener viajes
+     */
+    @Test
+    public void testobtenerViajes() throws Exception {
+        List<ViajeEntidadTemporal> viajesRetorno = viajesControlador.obtenerViajes();
+        assertNotNull(viajesRetorno);
+        assertEquals(viajesRetorno.size(), 3);
+    }
+
+    /**
+     * Metodo para obtener un usuario para las pruebas
+     * @return Retorna un objeto de tipo usuarioEntidad
+     */
+    public static UsuarioTemporal getUsuarioTemporal(){
+        UsuarioTemporal usuarioTemporal = new UsuarioTemporal();
+        usuarioTemporal.setCorreo("gerente11@gerente.com");
+        usuarioTemporal.setNombre("Gerente");
+        usuarioTemporal.setApellido1("Apellido1");
+        usuarioTemporal.setApellido2("Apellido2");
+        usuarioTemporal.setTelefono("11111111");
+        usuarioTemporal.setContrasena("$2a$10$gJ0hUnsEvTp5zyBVo19IHe.GoYKkL3Wy268wGJxG5.k.tUFhSUify");
+        usuarioTemporal.setFoto("foto");
+        usuarioTemporal.setIdGrupo("Cliente");
+        return usuarioTemporal;
+    }
+
+    /**
+     * Metodo para obtener un usuario para las pruebas
+     * @return Retorna un objeto de tipo usuarioTemporal
+     */
+    public static UsuarioEntidad getUsuarioEntidad(){
+        return getUsuarioTemporal().convertirAUsuarioEntidad();
+    }
 
 }
 
--- src/test/java/com/coopetico/coopeticobackend/controladores/unit/ViajesControladorUnitTest.java
+++ src/test/java/com/coopetico/coopeticobackend/controladores/unit/ViajesControladorUnitTest.java
@@ -8,29 +8,23 @@ package com.coopetico.coopeticobackend.controladores.unit;
  */
 
 import com.coopetico.coopeticobackend.controladores.ViajeControlador;
-import com.coopetico.coopeticobackend.entidades.bd.ViajeEntidad;
+import com.coopetico.coopeticobackend.entidades.UsuarioTemporal;
+import com.coopetico.coopeticobackend.entidades.ViajeEntidadTemporal;
+import com.coopetico.coopeticobackend.entidades.bd.*;
+import com.coopetico.coopeticobackend.servicios.UsuarioServicio;
 import com.coopetico.coopeticobackend.servicios.ViajesServicio;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.maps.model.LatLng;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
@@ -50,6 +44,9 @@ public class ViajesControladorUnitTest {
     @Autowired
     ViajeControlador viajesControlador;
 
+    @MockBean
+    UsuarioServicio usuarioServicio;
+
     // Mock
     @MockBean
     ViajesServicio viajeServicio;
@@ -67,10 +64,63 @@ public class ViajesControladorUnitTest {
     public static List<ViajeEntidad> getListaViajesEntidad(){
         List<ViajeEntidad> viajes = new ArrayList<>();
         Date date = new Date();
-        // viajes.add(new ViajeEntidad("AAA111", true, "11112222", "A", "Auto", new Timestamp(date.getTime()), new Timestamp(date.getTime()), new Timestamp(date.getTime()),null,"foto.jpg", true,"correo@correo.com",null));
+        ViajeEntidadPK viajePK = new ViajeEntidadPK();
+        TaxiEntidad taxiByPkPlacaTaxi = new TaxiEntidad();
+        ClienteEntidad clienteByPkCorreoCliente = new ClienteEntidad();
+        clienteByPkCorreoCliente.setPkCorreoUsuario("gerente11@gerente.com");
+        TaxistaEntidad taxistaByCorreoTaxi = new TaxistaEntidad();
+        taxistaByCorreoTaxi.setPkCorreoUsuario("gerente11@gerente.com");
+        OperadorEntidad agendaOperador = new OperadorEntidad();
+        agendaOperador.setPkCorreoUsuario("gerente11@gerente.com");
+
+        viajePK.setPkPlacaTaxi("CCC11");
+        viajePK.setPkFechaInicio(new Timestamp(date.getTime()));
+        viajes.add(new ViajeEntidad(viajePK, new Timestamp(date.getTime()),"5000", 2, "origen","destino", "agenda", "agenda2", taxiByPkPlacaTaxi, clienteByPkCorreoCliente, taxistaByCorreoTaxi, agendaOperador));
+        viajePK.setPkPlacaTaxi("DDD11");
+        viajes.add(new ViajeEntidad(viajePK, new Timestamp(date.getTime()),"5000", 2, "origen","destino", "agenda", "agenda2", taxiByPkPlacaTaxi, clienteByPkCorreoCliente, taxistaByCorreoTaxi, agendaOperador));
+        viajePK.setPkPlacaTaxi("EEE11");
+        viajes.add(new ViajeEntidad(viajePK, new Timestamp(date.getTime()),"5000", 2, "origen","destino", "agenda", "agenda2", taxiByPkPlacaTaxi, clienteByPkCorreoCliente, taxistaByCorreoTaxi, agendaOperador));
 
         return viajes;
     }
 
+    /**
+     * Test de obtener viajes
+     */
+    @Test
+    public void testobtenerViajes() throws Exception {
+        List<ViajeEntidad> viajes = getListaViajesEntidad();
+        given(viajeServicio.consultarViajes()).willReturn(viajes);
+        given(usuarioServicio.usuarioPorCorreo(getUsuarioEntidad().getPkCorreo())).willReturn(Optional.of(getUsuarioEntidad()));
+
+        List<ViajeEntidadTemporal> viajesRetorno = viajesControlador.obtenerViajes();
+        assertNotNull(viajesRetorno);
+        assertEquals(viajesRetorno.size(), 3);
+    }
+
+    /**
+     * Metodo para obtener un usuario para las pruebas
+     * @return Retorna un objeto de tipo usuarioEntidad
+     */
+    public static UsuarioTemporal getUsuarioTemporal(){
+        UsuarioTemporal usuarioTemporal = new UsuarioTemporal();
+        usuarioTemporal.setCorreo("gerente11@gerente.com");
+        usuarioTemporal.setNombre("Gerente");
+        usuarioTemporal.setApellido1("Apellido1");
+        usuarioTemporal.setApellido2("Apellido2");
+        usuarioTemporal.setTelefono("11111111");
+        usuarioTemporal.setContrasena("$2a$10$gJ0hUnsEvTp5zyBVo19IHe.GoYKkL3Wy268wGJxG5.k.tUFhSUify");
+        usuarioTemporal.setFoto("foto");
+        usuarioTemporal.setIdGrupo("Cliente");
+        return usuarioTemporal;
+    }
+
+    /**
+     * Metodo para obtener un usuario para las pruebas
+     * @return Retorna un objeto de tipo usuarioTemporal
+     */
+    public static UsuarioEntidad getUsuarioEntidad(){
+        return getUsuarioTemporal().convertirAUsuarioEntidad();
+    }
 }
 
