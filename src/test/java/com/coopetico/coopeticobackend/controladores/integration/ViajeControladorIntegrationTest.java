416da4356f10e37ab3583c644d1cd3fec022ca9f
Jose David Vargas <josed1608@gmail.com>
2019-05-21 16:23:40 -0600

    Comentarios de los tests y refactorización

--- src/test/java/com/coopetico/coopeticobackend/controladores/integration/ViajeControladorIntegrationTest.java
+++ src/test/java/com/coopetico/coopeticobackend/controladores/integration/ViajeControladorIntegrationTest.java
@@ -1,8 +1,5 @@
 package com.coopetico.coopeticobackend.controladores.integration;
 
-import com.coopetico.coopeticobackend.controladores.AuthControlador;
-import com.coopetico.coopeticobackend.controladores.UbicacionTaxistasControlador;
-import com.coopetico.coopeticobackend.controladores.ViajeControlador;
 import com.coopetico.coopeticobackend.entidades.DatosTaxistaAsigadoEntidad;
 import com.coopetico.coopeticobackend.entidades.ViajeComenzandoEntidad;
 import org.junit.Before;
@@ -10,17 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.stomp.StompFrameHandler;
 import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -38,15 +32,15 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static junit.framework.TestCase.assertEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
@@ -81,56 +75,60 @@ public class ViajeControladorIntegrationTest {
         URL = "ws://localhost:" + port + "/ws";
 
         // Obtener token de cliente
-        MvcResult result = mockMvc.perform(post("/auth/signin")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{" +
-                        "\"username\": \"cliente@cliente.com\"," +
-                        "\"password\": \"contrasenna\"" +
-                        "}"))
-                .andReturn();
-        String token = result.getResponse().getContentAsString();
-        authHeaderCliente = new HttpHeaders();
-        authHeaderCliente.add("Authorization", "Bearer " + token);
-
-        // Obtener token de taxista
-        result = mockMvc.perform(post("/auth/signin")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{" +
-                        "\"username\": \"taxista1@taxista.com\"," +
-                        "\"password\": \"contrasenna\"" +
-                        "}"))
-                .andReturn();
-        token = result.getResponse().getContentAsString();
-        authHeaderTaxista = new HttpHeaders();
-        authHeaderTaxista.add("Authorization", "Bearer " + token);
-
-        // Obtener token de taxista2
-        result = mockMvc.perform(post("/auth/signin")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{" +
-                        "\"username\": \"taxista2@taxista.com\"," +
-                        "\"password\": \"contrasenna\"" +
-                        "}"))
-                .andReturn();
-        token = result.getResponse().getContentAsString();
-        authHeaderTaxista2 = new HttpHeaders();
-        authHeaderTaxista2.add("Authorization", "Bearer " + token);
+        authHeaderCliente = obtenerToken("cliente@cliente.com", "contrasenna");
+        authHeaderTaxista = obtenerToken("taxista1@taxista.com", "contrasenna");
+        authHeaderTaxista2 = obtenerToken("taxista2@taxista.com", "contrasenna");
 
         // Cargar datos para test de la estructura
         mockMvc.perform(post("/ubicaciones/cargar-datos-test")).andReturn();
     }
 
-    @Test
-    @Transactional
-    public void solicitarViaje() throws Exception {
+    /**
+     * Método para generar el token correspondiente al usuario y contraseña dados, devuelve el header de autenticación ya armado
+     *
+     * @param usuario correo del usuario
+     * @param contrasenna contraseña
+     * @return retorna el header para autorizarse
+     */
+    private HttpHeaders obtenerToken(String usuario, String contrasenna) throws Exception {
+        MvcResult result = mockMvc.perform(post("/auth/signin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{" +
+                        "\"username\": \"" + usuario + "\"," +
+                        "\"password\": \"" + contrasenna + "\"" +
+                        "}"))
+                .andReturn();
+        String token = result.getResponse().getContentAsString();
+        HttpHeaders headerAuthorization = new HttpHeaders();
+        headerAuthorization.add("Authorization", "Bearer " + token);
+        return headerAuthorization;
+    }
+
+    /**
+     * Crea la conexión con el websocket utiliando el token suministrado
+     *
+     * @param headerAutorizacion header con el token de autorización
+     * @return retorna la sesión de la conexión con el WS
+     */
+    private StompSession obtenerSesionWS(HttpHeaders headerAutorizacion) throws InterruptedException, ExecutionException, TimeoutException {
         WebSocketStompClient stompClient = new WebSocketStompClient(new SockJsClient(createTransportClient()));
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 
-        StompSession stompSession = stompClient.connect(URL, new WebSocketHttpHeaders(authHeaderTaxista), new StompSessionHandlerAdapter() {
+        return stompClient.connect(URL, new WebSocketHttpHeaders(headerAutorizacion), new StompSessionHandlerAdapter() {
         }).get(1, SECONDS);
+    }
 
+    /**
+     * Testea que se solicite un viaje. El viaje se pide en plaza Lincoln, que es donde está el taxista1
+     */
+    @Test
+    @Transactional
+    public void solicitarViaje() throws Exception {
+        // Arrange: Que el taxista se suscriba a recibir viajes
+        StompSession stompSession = obtenerSesionWS(authHeaderTaxista);
         stompSession.subscribe("/user/queue/recibir-viaje", new ViajeControladorIntegrationTest.CreateStringStompFrameHandlerViajeSolicitado());
 
+        // Act: que el cliente solicite un viaje
         mockMvc.perform(get("/viajes/solicitar")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
@@ -140,23 +138,25 @@ public class ViajeControladorIntegrationTest {
                                     "\t\"tipo\": \"sedan\",\n" +
                                     "\t\"datafono\": true\n" +
                                 "}"))
+                // Assert: que se haya escogido un taxista
                 .andExpect(content().string("Se le avisó al primer taxista taxista1@taxista.com"));
 
+        // Assert: Que al taxista le haya llegado la notificación de viaje
         ViajeComenzandoEntidad notificacionTaxista = completableFutureViajeComenzado.get(10, SECONDS);
         assertEquals("cliente@cliente.com", notificacionTaxista.getCorreoCliente());
     }
 
+    /**
+     * Testea que el taxista acepte el viaje y se le avise al cliente
+     */
     @Test
     @Transactional
     public void aceptarViaje() throws Exception {
-        WebSocketStompClient stompClientCliente = new WebSocketStompClient(new SockJsClient(createTransportClient()));
-        stompClientCliente.setMessageConverter(new MappingJackson2MessageConverter());
-
-        StompSession stompSessionCliente = stompClientCliente.connect(URL, new WebSocketHttpHeaders(authHeaderCliente), new StompSessionHandlerAdapter() {
-        }).get(1, SECONDS);
-
+        // Arrange: que el cliente se suscriba a esperar taxista
+        StompSession stompSessionCliente = obtenerSesionWS(authHeaderCliente);
         stompSessionCliente.subscribe("/user/queue/esperar-taxista", new ViajeControladorIntegrationTest.CreateStringStompFrameHandlerTaxistaAsignado());
 
+        // Act: Que el taxista responda afirmativo al viaje
         mockMvc.perform(post("/viajes/aceptar-rechazar?respuesta=true")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
@@ -170,19 +170,22 @@ public class ViajeControladorIntegrationTest {
                 .headers(authHeaderTaxista))
             .andExpect(content().string("Viaje comienza"));
 
+        // Assert: Que al cliente le haya llegado la info del taxista1
         DatosTaxistaAsigadoEntidad notificacionTaxista = completableFutureTaxistaAsignado.get(10, SECONDS);
         assertEquals("taxista1@taxista.com", notificacionTaxista.getCorreoTaxista());
     }
 
+    /**
+     * Prueba que si un taxista rechaza un mensaje, que se le avise al siguiente taxista
+     */
     @Test
     @Transactional
     public void rechazarViaje() throws Exception {
-        WebSocketStompClient stompClientTaxista2 = new WebSocketStompClient(new SockJsClient(createTransportClient()));
-        stompClientTaxista2.setMessageConverter(new MappingJackson2MessageConverter());
-        StompSession stompSessionTaxista2 = stompClientTaxista2.connect(URL, new WebSocketHttpHeaders(authHeaderTaxista2), new StompSessionHandlerAdapter() {
-        }).get(1, SECONDS);
+        // Arrange, que el taxista al que le va a caer de segundo el vijae se suscriba
+        StompSession stompSessionTaxista2 = obtenerSesionWS(authHeaderTaxista2);
         stompSessionTaxista2.subscribe("/user/queue/recibir-viaje", new ViajeControladorIntegrationTest.CreateStringStompFrameHandlerViajeSolicitado());
 
+        // Act, que el cliente pida el viaje
         mockMvc.perform(post("/viajes/aceptar-rechazar?respuesta=false")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\n" +
@@ -196,6 +199,7 @@ public class ViajeControladorIntegrationTest {
                 .headers(authHeaderTaxista))
                 .andExpect(status().isOk());
 
+        // Assert: asegurarse que al segundo taxista le llegara el viaje
         ViajeComenzandoEntidad viajeTaxista2 = completableFutureViajeComenzado.get(10, SECONDS);
         assertEquals("cliente@cliente.com", viajeTaxista2.getCorreoCliente());
     }
