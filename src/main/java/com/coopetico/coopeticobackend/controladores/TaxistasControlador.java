65c85d2139f90755b4c4813099d355a4ddf1c92d
Jose David Vargas <josed1608@gmail.com>
2019-06-21 10:06:05 -0600

    Fix a error en TaxistasControlador que decía que no habían constructores visibles

--- src/main/java/com/coopetico/coopeticobackend/controladores/TaxistasControlador.java
+++ src/main/java/com/coopetico/coopeticobackend/controladores/TaxistasControlador.java
@@ -66,7 +66,7 @@ public class TaxistasControlador {
     /**
      * Constructor
      */
-    private TaxistasControlador(){
+    public TaxistasControlador(){
         this.utilidadesControlador = new UtilidadesControlador();
     }
 
